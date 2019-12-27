796ab1d2ba8e2e4fd77ba8657845e0edf7d0109a
Call me Joker <jump2160@gmail.com>
2019-12-27 13:49:06 +0300

    Added check system

--- geo_war/paiting_map/urls.py
+++ geo_war/paiting_map/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.mapindex, name='map'),
-    path('square_add/', views.SquareAdd, name='square_add')
+    path('square_add/', views.SquareAdd, name='square_add'),
+    path('square_check/', views.SquareSearch, name='square_check')
 ]
--- geo_war/paiting_map/views.py
+++ geo_war/paiting_map/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Scoreboard, Squard, Comand
 from . import APIRequests
 
@@ -10,5 +10,18 @@ def SquareAdd(request):
     Words = APIRequests.Get3Words(coordinates)
     Team = request.POST['team']
     print('Square created, word_1 = {0}, word_2 = {1}, word_3 = {2}, team = {3}'.format(Words['Word_1'],Words['Word_2'],Words['Word_3'],Team))
-    #Squard.Create(word_1 = Words['Word_1'], word_2 = Words['Word_2'], word_3 = Words['Word_3'], team = Team)
-    return render(request, 'mapindex.html',)
+    Sq = Squard(word_1 = Words['Word_1'], word_2 = Words['Word_2'], word_3 = Words['Word_3'])
+    Sq.save()
+    data = {'word_1': Words['Word_1'], 'word_2': Words['Word_2'], 'word_3': Words['Word_3'], 'type': 'add'}
+    return render(request, 'mapindex.html', data)
+
+def SquareSearch(request):
+    Word_1 = request.POST['word_1']
+    Word_2 = request.POST['word_2']
+    Word_3 = request.POST['word_3']
+    try:
+        a = Squard.objects.get(word_1 = Word_1, word_2 = Word_2, word_3 = Word_3)
+    except:
+        return HttpResponse('Something went wrong')
+    data = {"word_1": a.word_1,"word_2": a.word_2,"word_3": a.word_3, "type": 'check'}
+    return render(request, 'mapindex.html', data)
