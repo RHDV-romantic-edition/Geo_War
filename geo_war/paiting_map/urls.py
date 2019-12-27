220147bed3a7667400318aed58cc7b1135d735ed
MrHuge-coder <townhospis@gmail.com>
2019-12-27 14:01:08 +0300

    WhatTheWuck

--- geo_war/paiting_map/urls.py
+++ geo_war/paiting_map/urls.py
@@ -4,5 +4,7 @@ from . import views, functions
 
 urlpatterns = [
     path('', views.mapindex, name='map'),
+    path('Alex/', views.AlexWork, name='alex'),
     path('square_add/', views.SquareAdd, name='square_add')
 ]
+
--- geo_war/paiting_map/views.py
+++ geo_war/paiting_map/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Scoreboard, Squard, Comand
 from . import APIRequests
 
+def mapindex(request):
+    return render(request, 'mapindex.html',)
+
 def SquareAdd(request):
     coordinates = (request.POST['latitude'], request.POST['longitude'])
     Words = APIRequests.Get3Words(coordinates)
@@ -9,3 +12,6 @@ def SquareAdd(request):
     print('Square created, word_1 = {0}, word_2 = {1}, word_3 = {2}, team = {3}'.format(Words['Word_1'],Words['Word_2'],Words['Word_3'],Team))
     #Squard.Create(word_1 = Words['Word_1'], word_2 = Words['Word_2'], word_3 = Words['Word_3'], team = Team)
     return render(request, 'mapindex.html',)
+
+def AlexWork(request):
+    return render(request, 'test.html',)
