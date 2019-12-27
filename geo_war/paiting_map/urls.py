e9ef1c6aa57fec45de85c52966f7c96951d47e1b
Call me Joker <jump2160@gmail.com>
2019-12-27 14:20:29 +0300

    Right html added

--- geo_war/paiting_map/urls.py
+++ geo_war/paiting_map/urls.py
@@ -5,7 +5,6 @@ from . import views, functions
 urlpatterns = [
     path('', views.mapindex, name='map'),
     path('Alex/', views.AlexWork, name='alex'),
-    path('square_add/', views.SquareAdd, name='square_add')
+    path('square_add/', views.SquareAdd, name='square_add'),
     path('square_check/', views.SquareSearch, name='square_check')
 ]
-
