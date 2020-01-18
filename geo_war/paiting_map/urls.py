4b6b7159e42baf5525ebce8c93ce80e361420572
Call me Joker <jump2160@gmail.com>
2020-01-18 16:20:08 +0300

    Some fronted added

--- geo_war/paiting_map/urls.py
+++ geo_war/paiting_map/urls.py
@@ -4,7 +4,7 @@ from . import views, functions
 
 urlpatterns = [
     path('testmap', views.mapindex, name='test'),
-    path('', views.AlexWork, name='work'),
+    path('', views.LoginForm, name='work'),
+    path('login/', views.Login, name='work'),
     path('square_add/', views.SquareAdd, name='square_add'),
-    path('square_check/', views.SquareSearch, name='square_check'),
 ]
--- geo_war/paiting_map/views.py
+++ geo_war/paiting_map/views.py
@@ -6,45 +6,42 @@ import json
 def mapindex(request):
     return render(request, 'mapindex.html')
 
-
-def GetSq():
+def SquaresGet():
     data = []
     bse = {}
     for e in Squard.objects.all():
         data.append(e.__str__())
     for e in range(len(data)):
         a = data[e].split('.')
+        print(a)
         data[e] = APIRequests.GetCoordinates((a[0],a[1],a[2]))
-        bse[data[e]] = 'red'
-    print(bse)
-    return(bse)
-
-
-
+        bse[data[e]] = "#FF0000"
+    my_data = json.dumps(bse)
+    return my_data
 
 def SquareAdd(request):
     if request.method == 'POST':
         res = request.body.decode("utf-8")
         res = eval(res)
+        color_ = 'red'#res['color']
         res = res['cords']
     coordinates = (res['lat'], res['lng'])
     Words = APIRequests.Get3Words(coordinates)
     print('Square created, word_1 = {0}, word_2 = {1}, word_3 = {2}, team = lol'.format(Words['Word_1'],Words['Word_2'],Words['Word_3']))
-    Sq = Squard(word_1 = Words['Word_1'], word_2 = Words['Word_2'], word_3 = Words['Word_3'])
+    Sq = Squard(word_1 = Words['Word_1'], word_2 = Words['Word_2'], word_3 = Words['Word_3'], color = color_)
     Sq.save()
     data = {'word_1': Words['Word_1'], 'word_2': Words['Word_2'], 'word_3': Words['Word_3'], 'type': 'add'}
     return render(request, 'mapindex.html', data)
 
-def SquareSearch(request):
-    Word_1 = request.POST['word_1']
-    Word_2 = request.POST['word_2']
-    Word_3 = request.POST['word_3']
-    try:
-        a = Squard.objects.get(word_1 = Word_1, word_2 = Word_2, word_3 = Word_3)
-    except:
-        return HttpResponse('Something went wrong')
-    data = {"word_1": a.word_1,"word_2": a.word_2,"word_3": a.word_3, "type": 'check'}
-    return render(request, 'mapindex.html', data)
+def LoginForm(request):
+    return render(request, 'login.html')
+
+def Login(request):
+    Team = request.POST['team']
+    password = request.POST['password']
+    if password == '1111':
+        return render(request, 'test.html', {'my_data': SquaresGet(), 'type': 'Suc'})
+    return render(request, 'login.html', {'type': 'error'})
 
 def AlexWork(request):
     data = []
